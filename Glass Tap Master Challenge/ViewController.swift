de9d628c494e17b4b7f85d88baa4e379e187f2ad
Joe Donahue <Spiritjoe@gmail.com>
2016-03-14 17:48:26 -0500

    Initial Commit

---
+++ Glass Tap Master Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Glass Tap Master Challenge
+//
+//  Created by Joseph Donahue on 3/14/16.
+//  Copyright © 2016 Joseph Donahue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
